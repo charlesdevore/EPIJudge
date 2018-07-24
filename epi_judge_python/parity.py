070b1e6dd7ec2fa2fddcbcfd8645864838141d2d
charlesdevore <37390793+charlesdevore@users.noreply.github.com>
2018-07-24 10:29:44 -0400

    Brute force and improved algorithms

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,44 @@ from test_framework import generic_test
 
 
 def parity(x):
-    # TODO - you fill in here.
-    return 0
+
+    #return brute_force_parity(x)
+    return improved_parity(x)
+
+
+def improved_parity(x):
+
+    """Determine the parity of a binary number using an improved
+algorithm. The algorithm works by dropping the last bit in the number
+and then finding the XOR for the result. If the number of bits is odd,
+the while loop will execute an extra time resulting in a True result.
+
+    """
+
+    result = False
+    while x:
+        result ^= 1
+        x &= (x-1)
+
+    return result
+
+
+def brute_force_parity(x):
+    
+    """
+Determine the parity of a binary number. The parity is defined as
+true for an odd number of bits and false for an even number of bits.
+
+The function works by finding whether the current bit is true and then
+finding the XOR of the result.
+    """
+    
+    result = False
+    while x:
+        result ^= x & 1
+        x >>= 1
+        
+    return result
 
 
 if __name__ == '__main__':
