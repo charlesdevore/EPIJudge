da760a8f2b07071c0398d73e72e09fe5791bde47
charlesdevore <37390793+charlesdevore@users.noreply.github.com>
2018-08-13 11:40:46 -0400

    intersect_two_sorted_arrays()

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -2,8 +2,31 @@ from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A, B):
-    # TODO - you fill in here.
-    return []
+    """Return the intersection of two lists in O(n+m) time and 0(1)
+    space. Iterate through each array and if the values are the same
+    store at the beginning of the first array.
+    """
+
+    iA, iB, iout = 0,0,0
+
+    while iA < len(A) and iB < len(B):
+        if A[iA] == B[iB]:
+            if A[iA] != A[iout-1] or iout is 0:
+                A[iout] = A[iA]
+                iout += 1
+
+            iA += 1
+            iB += 1
+
+        elif A[iA] < B[iB]:
+            iA += 1
+
+        elif B[iB] < A[iA]:
+            iB += 1
+            
+        #print(iA, iB, iout)
+            
+    return A[:iout]
 
 
 if __name__ == '__main__':
